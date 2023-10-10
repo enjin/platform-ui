cec39e4746b8d754f9c2656437fd712fb096332e
zlayine <zlayine@enjin.io>
2023-10-10 19:36:05 +0300

    add canary

--- resources/js/util/index.ts
+++ resources/js/util/index.ts
@@ -144,7 +144,7 @@ const wcProjectId = 'a4b92f550ab3039f7e084a879882bc96';
 export const wcRequiredNamespaces = {
     polkadot: {
         methods: ['polkadot_signTransaction'],
-        chains: ['polkadot:3af4ff48ec76d2efc8476730f423ac07'],
+        chains: ['polkadot:3af4ff48ec76d2efc8476730f423ac07', 'polkadot:a37725fd8943d2a524cb7ecc65da438f'],
         events: [],
     },
 };
