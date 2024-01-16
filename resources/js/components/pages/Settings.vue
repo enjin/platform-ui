3773d81d5e7478a155726c48e81e026b71d5d723
zlayine <zlayine@enjin.io>
2024-01-16 13:13:53 +0300

    add wallet account to settings page

--- resources/js/components/pages/Settings.vue
+++ resources/js/components/pages/Settings.vue
@@ -73,12 +73,40 @@
                                 </Btn>
                             </div>
                         </div>
+                        <div v-if="walletAccount">
+                            <h1 class="text-xl">Wallet Account</h1>
+                            <p class="mt-1 text-sm text-gray-500 max-w-3xl">
+                                The platform depends on there being a funded wallet daemon account setup and connected
+                                to the platform and blockchain so it can receive transactions from the platform, sign
+                                them on your behalf and then broadcast them to the network. You can get the latest
+                                version of the wallet daemon from this repository:
+                                <a href="https://github.com/enjin/wallet-daemon" target="_blank" class="text-primary">
+                                    https://github.com/enjin/wallet-daemon</a
+                                >
+                                and an overview can be found here:
+                                <a
+                                    href="https://docs.enjin.io/enjin-platform/wallet-daemon"
+                                    target="_blank"
+                                    class="text-primary"
+                                >
+                                    https://docs.enjin.io/enjin-platform/wallet-daemon</a
+                                >
+                            </p>
+                            <div class="flex items-end space-x-4 mt-4">
+                                <FormInput
+                                    v-model="walletAccount"
+                                    class="sm:w-[430px] flex-1 sm:flex-none text-gray-400"
+                                    name="walletAccount"
+                                    disabled
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div class="flex flex-col space-y-4 mt-4">
                     <div class="flex justify-between">
-                        <h1 class="text-xl md:text-2xl">Settings</h1>
+                        <h1 class="text-xl">Settings</h1>
                     </div>
                     <FormCheckbox
                         v-model="advancedMode"
@@ -115,6 +143,7 @@ import { shortString, snackbarErrors } from '~/util';
 import CopyTextIcon from '../CopyTextIcon.vue';
 import LoadingCircle from '../LoadingCircle.vue';
 import ConfirmModal from '../ConfirmModal.vue';
+import { publicKeyToAddress } from '~/util/address';
 
 const router = useRouter();
 const appStore = useAppStore();
@@ -126,6 +155,7 @@ const loading = ref(appStore.user || !appStore.hasMultiTenantPackage ? false : t
 const creating = ref(false);
 const confirmModal = ref(false);
 const confirmModalName = ref();
+const walletAccount = computed(() => publicKeyToAddress(appStore.user.account || appStore.config.daemon));
 
 const tokens = computed(() => appStore.user?.apiTokens);
 
