28a8a328ec20bffd44975f25e4dfe7a3848f2273
zlayine <zlayine@enjin.io>
2024-03-28 15:12:35 +0000

    add support button

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,18 +20,5 @@
     </script>
 
 	@vite('resources/js/app.ts', 'vendor/platform-ui/build')
-
-    <script type="text/javascript">
-        function loadZendeskWidget() {
-            const script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.id = 'ze-snippet';
-            script.async = true;
-            script.src = 'https://static.zdassets.com/ekr/snippet.js?key=7eb863a6-7815-449a-a5bb-ce13ebdd89db';
-            document.getElementsByTagName('head')[0].appendChild(script);
-        }
-
-        setTimeout(loadZendeskWidget, 5000);
-    </script>
 </body>
 </html>
