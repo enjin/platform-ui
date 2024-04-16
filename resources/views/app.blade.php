06b1bd11153a549d1d3dec25a88d55b25e919e20
Zouheir Layine <58786497+zlayine@users.noreply.github.com>
2024-04-16 18:28:40 +0300

    [PLA-1725] add recaptcha to login and register (#94)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,13 @@
     <script>
         global = globalThis
         window.__dynamic_base__ = '/vendor/platform-ui/build';
-        window.bootstrap = { name: "{{ env('APP_NAME') }}", network: "{{ env('NETWORK') }}" , url: window.location.origin, daemon: "{{ Enjin\Platform\Support\Account::daemon()['public_key'] }}" }
+        window.bootstrap = { 
+            name: "{{ env('APP_NAME') }}",
+            network: "{{ env('NETWORK') }}",
+            url: window.location.origin,
+            daemon: "{{ Enjin\Platform\Support\Account::daemon()['public_key'] }}",
+            captcha_key: "{{ env("CAPTCHA_KEY") }}"
+        }
     </script>
 
 	@vite('resources/js/app.ts', 'vendor/platform-ui/build')
