ca810f405af4758ded2210d2e89152ca290890fe
zlayine <zlayine@enjin.io>
2024-04-17 12:07:39 +0100

    fix login

--- resources/js/graphql/query/auth/Login.ts
+++ resources/js/graphql/query/auth/Login.ts
@@ -1,3 +1,3 @@
-export default `query Login($email: String!, $password: String!, $recaptcha: String) {
+export default `query Login($email: String!, $password: String!, $recaptcha: String!) {
     Login(email: $email, password: $password, recaptchaResponse: $recaptcha)
   }`;
