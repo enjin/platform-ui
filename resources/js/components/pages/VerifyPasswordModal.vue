4acda5ed6df5764fbff6a3369196f47cd3cd323d
Abner Tudtud <114082473+enjinabner@users.noreply.github.com>
2024-05-28 14:56:52 +0800

    [PLA-1417] Add dusk id (#114)

--- resources/js/components/pages/VerifyPasswordModal.vue
+++ resources/js/components/pages/VerifyPasswordModal.vue
@@ -21,8 +21,8 @@
         </div>
 
         <div class="flex justify-end space-x-4 mt-6">
-            <Btn @click="closeModal">Cancel</Btn>
-            <Btn primary @click="confirm">Confirm</Btn>
+            <Btn dusk="passwordCancelBtn" @click="closeModal">Cancel</Btn>
+            <Btn dusk="passwordConfirmBtn" primary @click="confirm">Confirm</Btn>
         </div>
     </Modal>
 </template>
