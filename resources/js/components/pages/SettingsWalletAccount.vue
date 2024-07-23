171390ebd6a322575e043ed6a106facd58fcf7f7
Zouheir Layine <zlayine@enjin.io>
2024-07-23 08:01:19 +0100

    [PLA-1906] add wallet accounts (#137)

--- resources/js/components/pages/SettingsWalletAccount.vue
+++ resources/js/components/pages/SettingsWalletAccount.vue
@@ -13,13 +13,29 @@
                 https://docs.enjin.io/docs/wallet-daemon
             </a>
         </p>
-        <div class="flex items-end space-x-4 mt-4">
-            <FormInput
-                v-model="walletAccount"
-                class="sm:w-[430px] flex-1 sm:flex-none text-light-content dark:text-dark-content"
+        <div class="flex flex-col space-y-4 mt-4">
+            <label
+                v-if="walletAccount"
+                class="text-light-content dark:text-dark-content bg-light-surface-background dark:bg-dark-surface-background text-sm rounded-md mr-auto p-2 cursor-pointer"
                 name="walletAccount"
                 disabled
-            />
+                @click="copyText(walletAccount)"
+            >
+                {{ walletAccount }}
+            </label>
+            <span v-else-if="userWalletAccounts.length" class="text-lg font-bold text-light-content-strong dark:text-dark-content-strong">
+                Connected Wallets
+            </span>
+            <label
+                v-for="account in userWalletAccounts"
+                :key="account"
+                class="text-light-content dark:text-dark-content bg-light-surface-background dark:bg-dark-surface-background hover:bg-light-surface-brand text-sm rounded-md mr-auto p-2 cursor-pointer"
+                name="walletAccount"
+                disabled
+                @click="copyText(account)"
+            >
+                {{ account }}
+            </label>
         </div>
     </div>
 </template>
@@ -28,9 +44,17 @@
 import { computed } from 'vue';
 import { useAppStore } from '~/store';
 import { publicKeyToAddress } from '~/util/address';
-import FormInput from '../FormInput.vue';
+import snackbar from '~/util/snackbar';
 
 const appStore = useAppStore();
 
-const walletAccount = computed(() => publicKeyToAddress(appStore.user?.account || appStore.config.daemon));
+const walletAccount = computed(() =>
+    appStore.isMultiTenant ? publicKeyToAddress(appStore.user?.account) : appStore.config.daemon
+);
+const userWalletAccounts = computed(() => appStore.user?.walletAccounts || []);
+
+const copyText = (text: string) => {
+    navigator.clipboard.writeText(text);
+    snackbar.info({ title: 'Copied to clipboard!' });
+};
 </script>
