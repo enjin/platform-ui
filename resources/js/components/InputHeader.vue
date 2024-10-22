3d7588f5a923e9de4d6a2fc908b07512cfde5e8b
Zouheir Layine <zlayine@enjin.io>
2024-10-22 11:00:41 +0100

    update token create

--- resources/js/components/InputHeader.vue
+++ resources/js/components/InputHeader.vue
@@ -1,21 +1,29 @@
 <template>
-    <div class="flex items-center">
-        <label
-            v-if="label"
-            class="block text-sm font-medium leading-6 text-light-content-strong dark:text-dark-content-strong"
-        >
-            {{ label }} <span v-if="required" class="text-red-500">&nbsp;*</span>
-        </label>
-        <Tooltip class="!flex items-center" v-if="tooltip" :text="tooltip">
-            <QuestionMarkCircleIcon class="ml-1 w-4 h-4 cursor-pointer text-light-content dark:text-dark-content" />
-        </Tooltip>
-        <ReadMoreButton v-if="readmore && !description" class="ml-2" :readmore="readmore" :dusk="duskId" />
-    </div>
+    <div>
+        <div v-if="label" class="flex items-center">
+            <label
+                v-if="label"
+                class="block text-sm font-medium leading-6 text-light-content-strong dark:text-dark-content-strong"
+            >
+                {{ label }} <span v-if="required" class="text-red-500">&nbsp;*</span>
+            </label>
+            <Tooltip class="!flex items-center" v-if="tooltip" :text="tooltip">
+                <QuestionMarkCircleIcon class="ml-1 w-4 h-4 cursor-pointer text-light-content dark:text-dark-content" />
+            </Tooltip>
+            <ReadMoreButton v-if="readmore && !description" class="ml-2" :readmore="readmore" :dusk="duskId" />
+        </div>
 
-    <p class="mt-1 text-sm text-light-content dark:text-dark-content" v-if="description">
-        {{ description }}
-        <ReadMoreButton v-if="readmore" :readmore="readmore" :dusk="duskId" />
-    </p>
+        <p
+            v-if="description"
+            class="text-sm text-light-content dark:text-dark-content"
+            :class="{
+                'mt-1': label,
+            }"
+        >
+            {{ description }}
+            <ReadMoreButton v-if="readmore" :readmore="readmore" :dusk="duskId" />
+        </p>
+    </div>
 </template>
 
 <script setup lang="ts">
